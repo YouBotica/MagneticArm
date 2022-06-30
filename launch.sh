fc1c63a9e933d313131521f9d3762d3f11b543ce
Andres F Hoyos <andresfelipe3@hotmail.es>
2022-06-30 18:11:09 -0500

    Controller on dev...

---
+++ launch.sh
@@ -0,0 +1,3 @@
+colcon build
+source install/setup.bash
+ros2 launch magnetic_robot gazebo.launch.py
--- src/magnetic_robot/setup.py
+++ src/magnetic_robot/setup.py
@@ -16,6 +16,7 @@ setup(
         (os.path.join('share', package_name, 'urdf'),  glob('urdf/*')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -26,6 +27,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'position_controller = magnetic_robot.position_controller:main'
         ],
     },
 )
