f06c6a0fd7d49927c11f0890b82127e945af42b5
Andres F Hoyos <andresfelipe3@hotmail.es>
2022-07-02 18:17:33 -0500

    Fixed Simulink model

--- move_joints.m
+++ move_joints.m
@@ -6,3 +6,20 @@ chatterMsg = ros2message(chatterPub);
 chatterMsg.data = [ones(1,6)];
 
 send(chatterPub,chatterMsg)
+
+%% Continuous:
+A = 0;
+B = 0;
+C = 1;
+D = 1;
+
+L = place(A', C', [-30])'
+
+%% Discrete:
+
+A = 1;
+B = 0;
+C = 1;
+D = 1;
+
+L = place(A', C', [exp(-40*0.01)])'
